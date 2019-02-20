a79edcac28b1db75ada1ef72665de975b410537c
Chao Ye <Chao12.ye@gmail.com>
2019-02-20 13:38:31 -0800

    created model.py and imported necessary files

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,12 +1,19 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, Users
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+
+#route to root page
+@app.route('/')
+def display_form():
+    ''' '''
+    return render_template()
+
 
 connect_db(app)
 db.create_all()
